25791938f67e6d1059c004959bf3b4f44b2575e7
全栈小学生 <1518079521@qq.com>
2024-07-27 09:21:10 +0800

    update wap

--- niucloud/public/wap/assets/verify.6163e59b.js
+++
@@ -1,1 +0,0 @@
-import{bE as r}from"./index-d5155418.js";function e(e){return r.get("verify_records",e)}function t(){return r.get("check_verifier")}function n(e){return r.get(`get_verify_by_code/${e}`)}function o(e){return r.post(`verify/${e}`,{},{showErrorMessage:!0})}function s(e){return r.get(`verify_detail/${e}`,{},{showErrorMessage:!0})}export{n as a,s as b,e as c,t as g,o as v};
